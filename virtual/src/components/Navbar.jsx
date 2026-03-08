87de12c215b5e9dd6bc696fd324fe9251fde0ad5
lily0335 <laraibanwar00002@gmail.com>
2026-03-08 22:13:51 +0500

    commit

--- virtual/src/components/Navbar.jsx
+++ virtual/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border border-neutral-700/80">
         <div className="container px-4 mx-auto relative text-sm">
-            <div className="container flex items-center items-center">
+            <div className="container flex justify-center items-center">
                 <div className="flex items-center flex-shrink-0">
                     <img className="h-10 w-10 mr-2" src={logo} alt="logo" />
                     <span className="text-xl tracking-tight">VirtualR</span>
@@ -20,6 +20,15 @@ const Navbar = () => {
                         </li>
                     ))}
                 </ul>
+                <div className="hidden lg:flex justify-center space-x-12 items-center">
+                   <a href="#" className="py-2 px-3 border rounded-md">
+                        Sign In
+                   </a>
+                   <a href="#" className="bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md"
+                   >
+                        Create an account
+                   </a> 
+                </div>
             </div>
         </div>
     </nav>
