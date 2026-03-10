01b2fa448b96c4ba74957eec1bd9ce9b18d0beb7
lily0335 <laraibanwar00002@gmail.com>
2026-03-10 21:29:35 +0500

    commit

--- virtual/src/components/HeroSection.jsx
+++ virtual/src/components/HeroSection.jsx
@@ -3,7 +3,15 @@ import video1 from "../assets/videos/video1.mp4";
 import video2 from "../assets/videos/video2.mp4";
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
+    <div className="flex flex-col items-center mt-6 lg:mt-20">
+        <h1 className="text-4xl lg:text-6xl font-bold text-center tracking-wide">
+            VirtualR build tools 
+            <span>
+            for developers
+            </span>
+
+        </h1>
+    </div>
   )
 }
 
